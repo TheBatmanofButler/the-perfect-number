ff925284957cc3fa6e8d3f748821fbafec13988c
Ganesh Ravichandran <the.ganesh.ravichandran@gmail.com>
2017-09-16 19:14:06 -0400

    modularize slides

--- js/global.js
+++ js/global.js
@@ -7,18 +7,44 @@ var type = function (d) {
 
 d3.csv("../csv/dv_data/interactive_data.csv", type, function(companies) {
 
-  var companyNames = ['All companies'];
+  var companyNames = ['All companies'],
+      companiesIPS = [],
+      companiesYearsNoTax = {
+                          '8': [],
+                          '7': [],
+                          '6': [],
+                          '5': [],
+                          '4': [],
+                          '3': [],
+                          '2': [],
+                          '1': []
+                        };
 
-  data = companies.map(function(d)
-  {
-    companyNames.push(d.company_name);
+  companies.map(function (d) {
+    companyNames.push(d['company_name']);
+    if (d['ceo_comp_amt'] != '') {
+      companiesIPS.push(d);
+    }
+    var years_no_tax = d['years_no_tax']
+    if (d['years_no_tax'] != '0') {
+      companiesYearsNoTax[years_no_tax].push(d);
+    }
   });
 
-  populateDropdown(companyNames);
+  createSlides(companies, companiesYearsNoTax);
 
-	for (var i = 1; i < 9; i++) {
-		years[i] = [2,3]
-	};
+  // populateDropdown(companyNames);
 
-	loadBarData(companies);
+
+  // slide1(barGraph, width, y);
+  // loadBarData(companies);
+  // console.log(companiesIPS);
+  // loadBarData(companies, companiesYearsNoTax);
+  // loadBarData(companiesIPS);
+
+  // loadBarGraph(companies);
+
+  // populateBarGraph(x, y, barGraph, companies, companiesIPS);
+  // populateBarGraph(x, y, barGraph, companiesIPS);
+
 });
