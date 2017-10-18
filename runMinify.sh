c4d1e3ef9d453598221b0f2fcb1e47e36de59b57
Vaidehi Dalmia <vaidehidalmia@gmail.com>
2017-10-18 16:53:18 -0400

    add shell script

---
+++ runMinify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+minify js/src/slides.js -o js/dist/slides.js 
+minify js/src/d3extras.js -o js/dist/d3extras.js 
+minify js/src/barGraph.js -o js/dist/barGraph.js 
+minify js/src/barGraphUpdates.js -o js/dist/barGraphUpdates.js 
+minify js/src/slides.js -o js/dist/slides.js 
+minify js/src/proportion.js -o js/dist/proportion.js 
+minify js/src/info.js -o js/dist/info.js 
+minify js/src/dropdown.js -o js/dist/dropdown.js 
+minify js/src/about.js -o js/dist/about.js 
+minify js/src/global.js -o js/dist/global.js 
+minify js/src/main.js -o js/dist/main.js 
+minify js/src/slides.js -o js/dist/slides.js 
+minify js/src/slides.js -o js/dist/slides.js
